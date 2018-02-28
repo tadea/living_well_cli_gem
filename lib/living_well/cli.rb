8c2d4151cfb03b6df138199ccab117b75a446567
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-02-27 20:52:53 -0500

    created user input method

--- lib/living_well/cli.rb
+++ lib/living_well/cli.rb
@@ -1,20 +1,42 @@
 #CLI Controller
 class LivingWell::CLI
 
-def call
+ def call
    puts "Welcome to Living Well"
    puts " "
    menu
    puts "-------------------------"
    user_input
    goodbye 
-end
+ end
 
-def menu
+ def menu
   puts "---------Main Menu---------"
   puts "1. Nutrition Articles"
   puts "2. Exit"
   puts "---------------------------"
-end
+ end
+
+ def user_input
+    input = 'nil'
+    while input
+    puts "Enter '1' to see articles or enter '2' to exit:"
+    input = gets.strip.downcase
+
+     case input
+     when "1"
+    list_articles
+    when "2"
+    goodbye
+    exit 
+    else
+    puts "---------------------------"
+    puts "Wrong input, try again!"
+    menu
+    end
+   end
+ end
+
+
 
 end
